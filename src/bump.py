4f22ca9b27c05752c3bfac7857da5b1b10a059cb
Nikhil Akki <17634231+nikhilakki@users.noreply.github.com>
2023-04-24 07:25:36 +0530

    cli interface added

--- src/bump.py
+++ src/bump.py
@@ -2,7 +2,7 @@ import json
 import tomllib
 from enum import Enum
 from typing import Union
-from src.config import VERSION_FILE_PATH, VERSION_KEY
+from config import VERSION_FILE_PATH, VERSION_KEY
 
 
 class UpdateType(Enum):
