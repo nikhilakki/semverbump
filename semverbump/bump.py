a1730e7a30f1bf0201481ac538ea7e6ffcb11ad1
Nikhil Akki <17634231+nikhilakki@users.noreply.github.com>
2023-04-25 01:14:32 +0530

    Version Updated to 1.2.4

--- semverbump/bump.py
+++ semverbump/bump.py
@@ -63,7 +63,7 @@ def dump_json(version_data: dict, file_name: str):
 
 def dump_toml(version_data: dict, file_name: str):
     with open(file_name, "r") as f:
-        f.write(tomllib.dumps(version_data))
+        tomllib.dump(version_data, f)
 
 
 def get_version_from_dict(d: dict, version_path: str) -> str:
